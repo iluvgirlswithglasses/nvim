ccddd2136786a1b4bcd4d85af67f040c875363d2
iluvgirlswithglasses <luunamdat204@gmail.com>
2026-06-17 09:01:59 +0700

    peak cursor position display

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,6 +14,27 @@ M.base46 = {
   -- },
 }
 
+M.ui = {
+  statusline = {
+    modules = {
+      cursor = function()
+        local utils = require "nvchad.stl.utils"
+        local stl_config = require("nvconfig").ui.statusline
+        local sep_style = stl_config.separator_style
+        local separators = (type(sep_style) == "table" and sep_style) or utils.separators[sep_style]
+        local sep_l = separators["left"]
+
+        local cur = vim.fn.line "."
+        local total = vim.fn.line "$"
+        local pct = math.floor(cur / total * 100)
+        local col = vim.fn.virtcol "."
+
+        return "%#St_pos_sep#" .. sep_l .. "%#St_pos_icon# %#St_pos_text# " .. pct .. "%%/" .. col .. " "
+      end,
+    },
+  },
+}
+
 M.term = {
   sizes = { sp = 0.4 },
 }
