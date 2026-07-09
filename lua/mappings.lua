f16ab039510846ffc4d7d0ca385c8346465f67d9
iluvgirlswithglasses <luunamdat204@gmail.com>
2026-07-09 14:33:57 +0700

    cursor moving up & down wrapped lines

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- move by display line instead of actual line when wrap is on
+map({ "n", "v" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, desc = "Move down by display line" })
+map({ "n", "v" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, desc = "Move up by display line" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
